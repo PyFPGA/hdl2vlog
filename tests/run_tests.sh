a2a6dcf60d3c2d474eb0fcc2f1579d8bb0e50c6d
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2024-11-30 20:20:18 -0300

    Renamed from test (directory) to tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,6 +3,7 @@ set -e
 SLOG2VLOG=../hdl2vlog/slog2vlog.py
 VHDL2VLOG=../hdl2vlog/vhdl2vlog.py
 SFILES=../hdl/slog
+SFILES=../hdl/vlog
 OUTDIR=results
 
 python3 $SLOG2VLOG --top counter --output $OUTDIR/slog-counter.v $SFILES/counter.sv
