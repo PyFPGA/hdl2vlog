09f2e1f798d7d8391764a6b5c03de2991271579c
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2024-12-11 18:32:08 -0300

    Update main README
    And other small changes.

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# hdl2vlog
+# HDLconv
 
-> **WARNING:** this is a WIP project in an early stage (Sep, 2023).
+> **WARNING:** this is a WIP project in its early stages (as of Dec 2024).
 
-VHDL / System Verilog to Verilog converter, based on [Yosys](https://github.com/YosysHQ/yosys), and the plugins [ghdl-yosys-plugin](https://github.com/ghdl/ghdl-yosys-plugin) and [synlig](https://github.com/chipsalliance/synlig). It relies on [Docker](https://docs.docker.com/get-docker) and [PyFPGA containers](https://github.com/PyFPGA/containers).
+HDL converter (between VHDL, SystemVerilog and/or Verilog), based on [GHDL](https://github.com/ghdl/ghdl), [Yosys](https://github.com/YosysHQ/yosys), and the plugins [ghdl-yosys-plugin](https://github.com/ghdl/ghdl-yosys-plugin) and [yosys-slang](https://github.com/povik/yosys-slang).
+It relies on [Docker](https://docs.docker.com/get-docker) and [PyFPGA containers](https://github.com/PyFPGA/containers).
+
+* `vhdl2vhdl`: converts from a newer VHDL to VHDL'93.
+* `vhdl2vlog`: converts from VHDL to Verilog.
+* `slog2vlog`: converts from SystemVerilog to Verilog.
--- hdl2vlog/hdl2vlog.py
+++ hdl2vlog/hdl2vlog.py
@@ -1,29 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020-2023 Rodrigo A. Melo
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright (C) 2020-2024 PyFPGA Project
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
+#
 
 """hdl2vlog: VHDL/SV to Verilog converter"""
 
 import argparse
 import logging
+import shutil
 import subprocess
 import time
+
+from pathlib import Path
+from jinja2 import Environment, FileSystemLoader
 
 
 from __version__ import __version__ as version
@@ -42,7 +34,7 @@ def get_args(is_vhdl=False):
 
     prog= 'vhdl2vlog' if is_vhdl else 'slog2vlog'
     lang = 'VHDL' if is_vhdl else 'System Verilog'
-    description = f'{lang} to Verilog converter'
+    description = f'{lang} to Verilog compiler'
     parser = argparse.ArgumentParser(prog=prog, description=description)
 
     parser.add_argument(
@@ -50,14 +42,12 @@ def get_args(is_vhdl=False):
         action='version',
         version=f'hdl2vlog - v{version}'
     )
-
     parser.add_argument(
         '-o', '--output',
         metavar='PATH',
         default='converted.v',
         help='output file [converted.v]'
     )
-
     parser.add_argument(
         '-t', '--top',
         metavar='NAME',
@@ -65,7 +55,6 @@ def get_args(is_vhdl=False):
     )
 
     if is_vhdl:
-
         parser.add_argument(
             '--generic',
             metavar=('GENERIC', 'VALUE'),
@@ -73,22 +62,18 @@ def get_args(is_vhdl=False):
             nargs=2,
             help=f'specify a top-level Generic {_MULTIPLE}'
         )
-
         parser.add_argument(
             '--arch',
             metavar='ARCH',
             help='specify a top-level Architecture'
         )
-
         parser.add_argument(
             'files',
             metavar='FILE[,LIBRARY]',
             nargs='+',
             help='VHDL file/s (with an optional LIBRARY specification)'
         )
-
     else:
-
         parser.add_argument(
             '--param',
             metavar=('PARAM', 'VALUE'),
@@ -96,7 +81,6 @@ def get_args(is_vhdl=False):
             nargs=2,
             help=f'specify a top-level Parameter {_MULTIPLE}'
         )
-
         parser.add_argument(
             '--define',
             metavar=('DEFINE', 'VALUE'),
@@ -104,14 +88,12 @@ def get_args(is_vhdl=False):
             nargs=2,
             help=f'specify a Define {_MULTIPLE}'
         )
-
         parser.add_argument(
             '--include',
             metavar='PATH',
             action='append',
             help=f'specify an Include Path {_MULTIPLE}'
         )
-
         parser.add_argument(
             'files',
             metavar='FILE',
@@ -121,14 +103,10 @@ def get_args(is_vhdl=False):
 
     return parser.parse_args()
 
-def get_template(name):
-    template = Path(__file__).parent / 'templates' / name
-    with template.open('r', encoding='utf-8') as fp:
-        return fp.read().strip()
-
 def run(command):
     start = time.time()
-    # TODO: which docker
+    if not shutil.which('docker'):
+        _log.error('docker is not available')
     try:
         subprocess.run(
             command, shell=True, check=True, universal_newlines=True,
@@ -136,6 +114,18 @@ def run(command):
     finally:
         end = time.time()
         _log.info('executed in %.3f seconds', end-start)
+
+def create_script(hdl, data):
+    tempdir = Path(__file__).parent.joinpath('templates')
+    jinja_file_loader = FileSystemLoader(str(tempdir))
+    jinja_env = Environment(loader=jinja_file_loader)
+    jinja_template = jinja_env.get_template(f'{hdl}.jinja')
+    content = jinja_template.render(data)
+    directory = Path(data['odir'])
+    directory.mkdir(parents=True, exist_ok=True)
+    with open(directory / f'{hdl}.sh', 'w', encoding='utf-8') as file:
+        file.write(content)
+
 
 def slog2vlog():
     args = get_args(is_vhdl=False)
@@ -155,12 +145,20 @@ def slog2vlog():
 def vhdl2vlog():
     args = get_args(is_vhdl=True)
     file = args.files[0]
-    cmd = get_template('yosys').format(
-        includes='',
-        defines='',
-        files=f'ghdl {file} -e',
-        params='',
-        top=args.top,
-        output=args.output
-    )
+    data = {
+        'top': args.top,
+        'arch': args.arch,
+        'output': args.output
+    }
+    for file in args.files:
+        aux = file.split(',')
+        file = aux[0]
+        lib = aux[1] if len(aux) > 1 else None
+        data.setdefault('files', {})[file] = lib
+    if args.generic is not None:
+        for generic in args.generic:
+            data.setdefault('generics', {})[generic[0]] = generic[1]
+    print(data)
+    exit(1)
+    create_script('vhdl', data)
     run(cmd)
